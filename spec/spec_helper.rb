e85beaf21a84eba764a28640b61f21aaedd36a74
Keith Avery <slabgorb@gmail.com>
2015-04-07 19:25:19 -0400

    removed dup code from wrong file, oops

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,4 @@
 require_relative "../reverse-polish-calculator.rb"
-describe ReversePolishCalculator do
-  before :each do
-    @calc = ReversePolishCalculator.new
-  end
-
-  it "handles problems with no input" do
-    expect{ @calc.parse('')}.to raise_error
-  end
-
-
-
-
-end
 
 
 
